3631227ea2e2a6355d4a65de5a74b729ef1951a5
JaviPatron <jpatronmf@gmail.com>
2023-04-14 11:23:05 -0700

    adding the git ignore and a new R Script

---
+++ no_functions.R
@@ -0,0 +1,17 @@
+# load packages 
+library(tidyverse)
+library(chron)
+library(naniar)
+library(ggridges)
+
+#Load in the data
+alegria <- read_csv("/Users/javipatron/Documents/MEDS/Courses/workshops/functions_workshop/data/raw_data/alegria_mooring_ale_20210617.csv")
+
+carpinteria <- read_csv("/Users/javipatron/Documents/MEDS/Courses/workshops/functions_workshop/data/raw_data/carpinteria_mooring_car_20220330.csv")
+
+mohawk <- read_csv("/Users/javipatron/Documents/MEDS/Courses/workshops/functions_workshop/data/raw_data/mohawk_mooring_mko_20220330.csv")
+
+
+
+
+
